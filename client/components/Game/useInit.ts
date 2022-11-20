125284e555c8e6a944c57a192b6c1a064c011481
Robert Coe <chakrawc@gmail.com>
2022-11-20 08:04:42 -0800

    fix: place useInit listeners in useEffect

--- client/components/Game/useInit.ts
+++ client/components/Game/useInit.ts
@@ -1,4 +1,4 @@
-import { useContext } from "preact/hooks";
+import { useContext, useEffect } from "preact/hooks";
 import { offsets } from "../../../common/constants.ts";
 import { newGrid } from "../../../common/pathing.ts";
 import {
@@ -30,68 +30,71 @@ export const useInit = () => {
     rating: lastRating,
   } = useContext(GameStateContext);
 
-  const startCallback = (
-    { checkpoint, blocks, thunders, ...event }: StartMessage,
-  ) => {
-    setLastRating(NaN);
-    setCheckpoint(checkpoint);
-    setThunders(thunders);
-    setBlocks(blocks);
-    setBricks(event.bricks);
-    setPower(event.power);
-    setTime(Math.floor(event.time));
-    setRating(event.rating);
+  useEffect(() => {
+    const startCallback = (
+      { checkpoint, blocks, thunders, ...event }: StartMessage,
+    ) => {
+      setLastRating(NaN);
+      setCheckpoint(checkpoint);
+      setThunders(thunders);
+      setBlocks(blocks);
+      setBricks(event.bricks);
+      setPower(event.power);
+      setTime(Math.floor(event.time));
+      setRating(event.rating);
 
-    grid.splice(0, Infinity, ...newGrid());
+      grid.splice(0, Infinity, ...newGrid());
 
-    grid[checkpoint.y + 0.5][checkpoint.x + 0.5] = true;
-    for (const { x, y } of thunders) {
-      offsets.forEach(([xd, yd]) => grid[y + yd][x + xd] = true);
-    }
-    for (const { x, y } of blocks) {
-      offsets.forEach(([xd, yd]) => grid[y + yd][x + xd] = true);
-    }
-  };
-  connection.addEventListener("start", startCallback);
+      grid[checkpoint.y + 0.5][checkpoint.x + 0.5] = true;
+      for (const { x, y } of thunders) {
+        offsets.forEach(([xd, yd]) => grid[y + yd][x + xd] = true);
+      }
+      for (const { x, y } of blocks) {
+        offsets.forEach(([xd, yd]) => grid[y + yd][x + xd] = true);
+      }
+    };
+    connection.addEventListener("start", startCallback);
 
-  const disconnectCallback = () => {
-    setDisconnected(true);
-    setTime(-1);
-  };
-  connection.addEventListener("disconnect", disconnectCallback);
+    const disconnectCallback = () => {
+      setDisconnected(true);
+      setTime(-1);
+    };
+    connection.addEventListener("disconnect", disconnectCallback);
 
-  const connectCallback = () => {
-    setDisconnected(false);
-    setCheckpoint({ x: -2, y: -2 });
-    setBlocks([]);
-    setThunders([]);
-    setBricks(0);
-    setPower(0);
-    setRun(undefined);
-    setInvalid(false);
-    setTransitionBlock(undefined);
-    setPlacingBlock((pb) => ({ ...pb, placing: false }));
-  };
-  connection.addEventListener("connect", connectCallback);
+    const connectCallback = () => {
+      setDisconnected(false);
+      setCheckpoint({ x: -2, y: -2 });
+      setBlocks([]);
+      setThunders([]);
+      setBricks(0);
+      setPower(0);
+      setRun(undefined);
+      setInvalid(false);
+      setTransitionBlock(undefined);
+      setPlacingBlock((pb) => ({ ...pb, placing: false }));
+    };
+    connection.addEventListener("connect", connectCallback);
 
-  const runCallback = ({ path, duration, slows, rating }: RunMessage) => {
-    setRun({ path, duration, slows });
-    setPlacingBlock((pb) => ({ ...pb, placing: false }));
-    setTransitionBlock(undefined);
-    setTime(-1);
-    setBricks(-1);
-    setPower(-1);
-    setTouching(false);
-    setThunderHover(undefined);
-    setLastRating(lastRating);
-    setRating(rating);
-  };
-  connection.addEventListener("run", runCallback);
+    const runCallback = ({ path, duration, slows, rating }: RunMessage) => {
+      console.warn("run callback");
+      setRun({ path, duration, slows });
+      setPlacingBlock((pb) => ({ ...pb, placing: false }));
+      setTransitionBlock(undefined);
+      setTime(-1);
+      setBricks(-1);
+      setPower(-1);
+      setTouching(false);
+      setThunderHover(undefined);
+      setLastRating(lastRating);
+      setRating(rating);
+    };
+    connection.addEventListener("run", runCallback);
 
-  return () => {
-    connection.removeEventListener("start", startCallback);
-    connection.removeEventListener("disconnect", disconnectCallback);
-    connection.removeEventListener("connect", connectCallback);
-    connection.removeEventListener("run", runCallback);
-  };
+    return () => {
+      connection.removeEventListener("start", startCallback);
+      connection.removeEventListener("disconnect", disconnectCallback);
+      connection.removeEventListener("connect", connectCallback);
+      connection.removeEventListener("run", runCallback);
+    };
+  }, []);
 };
